783fb60f7e8e50bbe69cb0fbd76dfd041d190067
nociza <azicon@berkeley.edu>
2022-05-04 00:21:24 -0700

    added more boilerplate

--- ethereum/contracts/HelloWorld.sol
+++ ethereum/contracts/HelloWorld.sol
@@ -2,6 +2,9 @@
 // Learn more: https://solidity.readthedocs.io/en/v0.5.10/layout-of-source-files.html#pragma
 pragma solidity ^0.7.0;
 
+
+import “stringUtils.sol”;
+
 // Defines a contract named `HelloWorld`.
 // A contract is a collection of functions and data (its state). Once deployed, a contract resides at a specific address on the Ethereum blockchain. Learn more: https://solidity.readthedocs.io/en/v0.5.10/structure-of-a-contract.html
 contract HelloWorld {
